578d229ac60075864b25f4be475c9a3c83a3a4b8
hwangbj123 <bj09289612@gmail.com>
2023-10-27 12:06:37 +0900

    결제 테스트

--- src/main/java/com/tencoding/ADayOfLearning/service/PaymentService.java
+++ src/main/java/com/tencoding/ADayOfLearning/service/PaymentService.java
@@ -3,6 +3,7 @@ package com.tencoding.ADayOfLearning.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tencoding.ADayOfLearning.dto.request.PaymentRequestDto;
 import com.tencoding.ADayOfLearning.repository.interfaces.PaymentRepository;
 
 @Service
@@ -10,4 +11,11 @@ public class PaymentService {
 	
 	@Autowired
 	PaymentRepository paymentRepository;
+	
+	
+	public int insertPayment(PaymentRequestDto paymentRequestDto) {
+		
+		return 0;
+	}
+	
 }
