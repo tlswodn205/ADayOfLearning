0e693073b7dd325b1218660e427faf86e54b8154
hwangbj123 <bj09289612@gmail.com>
2023-10-31 11:19:47 +0900

    예약리스트view

--- src/main/java/com/tencoding/ADayOfLearning/controller/ReserveController.java
+++ src/main/java/com/tencoding/ADayOfLearning/controller/ReserveController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tencoding.ADayOfLearning.dto.request.ReserveRequestDto;
 import com.tencoding.ADayOfLearning.dto.response.ReserveListResponseDto;
+import com.tencoding.ADayOfLearning.dto.response.ReserveResponseDto;
 import com.tencoding.ADayOfLearning.repository.model.Reserve;
 import com.tencoding.ADayOfLearning.service.ReserveService;
 
@@ -39,7 +40,7 @@ public class ReserveController {
 	
 	@GetMapping("/detail/{id}")
 	public String reserveDetail(Model model, @PathVariable Integer id) {
-		Reserve reserve = reserveService.findReserveByReserveId(id);
+		ReserveResponseDto reserve = reserveService.findReserveByReserveId(id);
 		model.addAttribute("reserve", reserve);
 		return "reserve/reserveDetail";
 	}
